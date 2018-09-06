2d98c84c7263ac2597e0d9e9e5735fe86b1b5e78
olimeme <39192645+olimeme@users.noreply.github.com>
2018-09-06 17:15:53 +0600

    Add files via upload

---
+++ TryCatch/TryCatch/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using static System.Console;
+
+namespace Namespace
+{
+    class Program
+    {
+        public static void ExecptionMethod()
+        {
+            throw new MethodAccessException();
+        }
+        static void Main(string[] args)
+        {
+            try
+            {
+                ExecptionMethod();
+            }
+            catch(MethodAccessException exception)
+            {
+                WriteLine(exception.Message);
+            }
+            ReadLine();
+        }
+    }
+}
