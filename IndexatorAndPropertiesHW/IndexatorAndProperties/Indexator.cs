d3f5c8845c32deb39e28a4238e9470f55eb6bcb8
olimeme <39192645+olimeme@users.noreply.github.com>
2018-09-10 21:13:03 +0600

    Add files via upload

---
+++ IndexatorAndPropertiesHW/IndexatorAndProperties/Indexator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndexatorAndProperties
+{
+    class Indexator
+    {
+        private int[] array = new int[5];
+        public int this[int index]
+        {
+            get
+            {
+                return array[index];
+            }
+            set
+            {
+                array[index] = value*value;
+            }
+        }
+    }
+}
---
+++ IndexatorAndPropertiesHW/IndexatorAndProperties/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndexatorAndProperties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Indexator indexators = new Indexator();
+            indexators[0] = 5;
+            Console.WriteLine(indexators[0]);
+            Console.ReadLine();
+        }
+    }
+}
