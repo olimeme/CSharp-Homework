8de172f9352ecdde8a7a9b74d99440d9d19c2d08
olimeme <39192645+olimeme@users.noreply.github.com>
2018-09-11 17:57:55 +0600

    Add files via upload

---
+++ IndexatorAndProperPW/IndexatorAndProperPW/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+namespace IndexatorAndProperPW
+{
+    class Program
+    {
+        static void PrintArray(RangeOfArray array)
+        {
+            for(int i=0;i<array.UpperIndex - array.LowerIndex;i++)
+            {
+                WriteLine(array.array[i]+" ");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int lowerIndex;
+
+            int upperIndex;
+
+            int element;
+
+            int elementIndex;
+
+            WriteLine("Введите индексный диапозон массива");
+
+            WriteLine("Введите верхний индекс ");
+            string str = ReadLine();
+            bool isParsed = int.TryParse(str, out upperIndex);
+                if (isParsed)
+                    upperIndex = int.Parse(str);
+
+            WriteLine("Введите нижний индекс ");
+            str = ReadLine();
+            isParsed = int.TryParse(str, out lowerIndex);
+                if (isParsed)
+                    lowerIndex = int.Parse(str);
+
+            RangeOfArray array = new RangeOfArray(lowerIndex, upperIndex);
+
+            WriteLine("Введите входной элемент");
+            str = ReadLine();
+            isParsed = int.TryParse(str, out element);
+            if (isParsed)
+                element = int.Parse(str);
+
+            WriteLine("Введите индекс элемента(от " + lowerIndex + " до " + upperIndex + ")");
+            str = ReadLine();
+            isParsed = int.TryParse(str, out elementIndex);
+            if (isParsed)
+                elementIndex = int.Parse(str);
+
+            array.Insert(element, elementIndex);
+            PrintArray(array);
+            ReadLine();
+        }
+    }
+}
---
+++ IndexatorAndProperPW/IndexatorAndProperPW/RangeOfArray.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndexatorAndProperPW
+{
+    class RangeOfArray
+    {
+        public int UpperIndex { get; set; }
+
+        public int LowerIndex { get; set; }
+
+        public int[] array { get; set; }
+
+        public RangeOfArray(int lowerIndex,int upperIndex)
+        {
+            if (upperIndex > lowerIndex)
+            {
+                UpperIndex = upperIndex;
+                LowerIndex = lowerIndex;
+                array = new int[UpperIndex - LowerIndex];
+            }
+            else
+            {
+                UpperIndex = lowerIndex;
+                LowerIndex = upperIndex;
+                array = new int[UpperIndex - LowerIndex];
+            }
+        }
+
+        public void Insert(int element,int index)
+        {
+            if (array.Length == 0||index<LowerIndex||index>UpperIndex)
+            {
+                Console.WriteLine("Введен индекс за пределами массива или длина массива равна 0");
+                return;
+            }
+            array[index-1] = element;
+        }
+
+    }
+}
