a2998996db2f409b4ba634b75a298db8b6fa2fd1
olimeme <39192645+olimeme@users.noreply.github.com>
2018-10-08 17:43:03 +0600

    Add files via upload

---
+++ GenericPW/GenericPW/CountTheWords.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericPW
+{
+    public class CountTheWords
+    {
+        private Dictionary<string, int> KeyValuePairs;
+
+        public Dictionary<string,int> GetKeyValuePairs()
+        {
+            return KeyValuePairs;
+        }
+
+        public void SetKeyValuePairs(Dictionary<string,int> keyValues)
+        {
+            KeyValuePairs = keyValues;
+        }
+
+        public CountTheWords()
+        {
+            KeyValuePairs = new Dictionary<string, int>();
+        }
+
+        public void AddWord(string word)
+        {
+            if (KeyValuePairs.ContainsKey(word))
+                KeyValuePairs[word] += 1;
+            else
+                KeyValuePairs.Add(word, 1);
+        }
+
+        public int AmountOfWords()
+        {
+            return KeyValuePairs.Count;
+        }
+    }
+}
---
+++ GenericPW/GenericPW/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericPW
+{
+    class Program
+    {
+        public static void PrintStats(CountTheWords countTheWords)
+        {
+            foreach (var i in countTheWords.GetKeyValuePairs())
+                Console.WriteLine("Слово " + i.Key + " встречается " + i.Value + " раз.");
+            Console.WriteLine("Всего уникальных слов: " + countTheWords.AmountOfWords());
+        }
+        static void Main(string[] args)
+        {
+            string s = "Вот дом, Который построил Джек. А это пшеница, Кото­рая в темном  чулане хранится В доме, Который построил Джек. А это веселая птица­ синица, Которая часто вору­ет пшеницу, Которая в темном чулане хранится В доме, Который построил Джек.";
+            char[] delimiters = { ' ', ',', '.','-'};
+            string[] words = s.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+            CountTheWords countTheWords = new CountTheWords();
+            for(int i=0;i<words.Length;i++)
+            {
+                countTheWords.AddWord(words[i]);
+            }
+            PrintStats(countTheWords);
+            Console.WriteLine("Всего слов:" + words.Length);
+            Console.ReadLine();
+        }
+    }
+}
