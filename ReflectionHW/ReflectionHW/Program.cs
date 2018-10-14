428ca4689cdc007aca836358d3d9b13337235f39
olimeme <39192645+olimeme@users.noreply.github.com>
2018-10-14 11:26:45 +0600

    Add files via upload

---
+++ ReflectionHW/ReflectionHW/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace ReflectionHW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task1();
+            Task2();
+        }
+
+        private static void Task2()
+        {
+            var testClass = new TestReflectionClass();
+
+            Type type = testClass.GetType();
+
+            var fields = type.GetFields(BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public);
+
+            foreach (var fieldInfo in fields)
+                Console.WriteLine("{0}: {1}", fieldInfo.Name, fieldInfo.FieldType);
+
+            Console.ReadLine();
+        }
+
+        private static void Task1()
+        {
+            Type type = typeof(Console);
+
+            var fields = type.GetMethods();
+
+            foreach (var field in fields)
+                Console.WriteLine(field.Name + field.ReflectedType);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ReflectionHW/ReflectionHW/TestReflectionClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReflectionHW
+{
+    public class TestReflectionClass
+    {
+        private string FirstName;
+        private string LastName;
+        private int Age;
+        public string Description;
+
+    }
+}
